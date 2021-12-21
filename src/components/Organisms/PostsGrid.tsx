bedb071696a4ce26691ec3002a79c59b8674a642
kwhong95 <kwhong95@kakao.com>
2021-12-21 15:42:40 +0900

    Get Posts Data - Apollo Client

--- src/components/Organisms/PostsGrid.tsx
+++ src/components/Organisms/PostsGrid.tsx
@@ -1,6 +1,19 @@
 import Atoms from "../Atoms";
+import { useQuery } from "@apollo/client";
+import { GET_POSTS } from "../../services/query";
+import type { IResult } from "../../services/query";
+import { PostCard } from "../Molecules";
 
 const PostsGrid = () => {
+  const { data, loading, error } = useQuery<IResult | null>(GET_POSTS);
+  const posts =
+    data?.postsConnection.edges.length !== 0
+      ? data?.postsConnection.edges
+      : null;
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error!</div>;
+
   return (
     <Atoms.Div
       display="grid"
@@ -8,9 +21,9 @@ const PostsGrid = () => {
       gap="12rem"
     >
       <Atoms.Div gridColumn="span 1 / span 1">
-        {/* {posts.map((post: any) => (
-            <PostCard post={post.node} key={post.title} />
-          ))} */}
+        {posts?.map((post: any) => (
+          <PostCard post={post.node} key={post.title} />
+        ))}
       </Atoms.Div>
       <Atoms.Div gridColumn="span 1 / span 1">
         <Atoms.Div gridColumn="span 1 / span 1">
--- src/services/query.ts
+++ src/services/query.ts
@@ -1,5 +1,39 @@
 import { gql } from 'apollo-boost';
 
+export interface IAuthor {
+  id: string;
+  bio: string;
+  name: string;
+  photo: {
+    url: string;
+  }
+}
+
+export interface ICategory {
+  name: string;
+  slug: string;
+}
+
+export interface INode {
+  author: IAuthor;
+  createAt: string;
+  slug: string;
+  title: string;
+  excerpt: string;
+  featuredImage: {
+    url: string;
+  };
+  categories: ICategory[]
+}
+
+export interface IEdges {
+  edges: INode[];
+}
+
+export interface IResult {
+  postsConnection: IEdges;
+}
+
 export const GET_POSTS = gql`
   query MyQuery {
   postsConnection {
@@ -28,4 +62,4 @@ export const GET_POSTS = gql`
     }
   }
 }
-` 
+`;
